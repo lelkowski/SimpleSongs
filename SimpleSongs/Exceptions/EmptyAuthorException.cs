61580272e72ba8e402698a09d6b1b60c77486dfa
Pawel Lelek <lelkowski331@gmail.com>
2022-06-06 16:07:07 +0200

    Add EmptyAuthorException

---
+++ SimpleSongs/Exceptions/EmptyAuthorException.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleSongs.Exceptions
+{
+    public class EmptyAuthorException : Exception
+    {
+        public EmptyAuthorException()
+        {
+
+        }
+
+        public EmptyAuthorException(string? message) : base(message)
+        {
+        }
+
+        public EmptyAuthorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
