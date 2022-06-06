789f7a76f33ad69e2c91b8a01fdf75b2144df601
Pawel Lelek <lelkowski331@gmail.com>
2022-06-06 16:04:59 +0200

    Add SongDao

---
+++ Database/DAO/SongDao.cs
@@ -0,0 +1,50 @@
+﻿using Database.Context;
+using Database.DAO.Interface;
+using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DAO
+{
+    public class SongDao : ISongDao
+    {
+        private SongsContext _context;
+
+        public SongDao(SongsContext context)
+        {
+            _context = context;
+        }
+
+        public List<Song> GetAllSongs()
+        {
+            return _context.Songs.ToList();
+        }
+
+        public Song GetSong(Func<Song, bool> condition)
+        {
+            return _context.Songs.FirstOrDefault(condition);
+        }
+
+        public void DeleteSong(Song songToDelete)
+        {
+            _context.Songs.Remove(songToDelete);
+            _context.SaveChanges();
+        }
+
+        public void UpdateSong(Song songToUpdate)
+        {
+            _context.Songs.Update(songToUpdate);
+            _context.SaveChanges();
+        }
+
+        public void AddSong(Song songToAdd)
+        {
+            _context.Songs.Add(songToAdd);
+            _context.SaveChanges();
+        }
+    }
+}
