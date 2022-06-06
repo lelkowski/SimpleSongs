70c4b686ee6f2d30a5492f6ed57a3c7881e10c75
Pawel Lelek <lelkowski331@gmail.com>
2022-06-06 16:04:15 +0200

    Add DAO

---
+++ Database/DAO/Interface/ISongDao.cs
@@ -0,0 +1,13 @@
+﻿using Database.Entities;
+
+namespace Database.DAO.Interface
+{
+    public interface ISongDao
+    {
+        void DeleteSong(Song songToDelete);
+        List<Song> GetAllSongs();
+        Song GetSong(Func<Song, bool> condition);
+        void UpdateSong(Song songToUpdate);
+        void AddSong(Song songToAdd);
+    }
+}
