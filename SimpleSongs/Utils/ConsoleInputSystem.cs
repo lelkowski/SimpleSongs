50fad48659a0cbfa1f9cbac84d8f8860f85a2bb7
Pawel Lelek <lelkowski331@gmail.com>
2022-06-06 16:06:05 +0200

    Add InputSystem

---
+++ SimpleSongs/Utils/ConsoleInputSystem.cs
@@ -0,0 +1,38 @@
+﻿using SimpleSongs.Utils.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongs.Utils
+{
+    public class ConsoleInputSystem : IInputSystem
+    {
+
+        public string FetchStringValue(string prompt)
+        {
+            string result = "";
+            while (result == "")
+            {
+                Console.WriteLine("\n\t" + prompt);
+                Console.Write("\t");
+                result = Console.ReadLine();
+            }
+            return result;
+        }
+
+        public double FetchDoubleValue(string prompt)
+        {
+            double value;
+            while (!Double.TryParse(FetchStringValue(prompt), out value)) continue;
+            return value;
+        }
+        public int FetchIntValue(string prompt)
+        {
+            int value;
+            while (!Int32.TryParse(FetchStringValue(prompt), out value)) continue;
+            return value;
+        }
+    }
+}
---
+++ SimpleSongs/Utils/Interfaces/IInputSystem.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleSongs.Utils.Interfaces
+{
+    public interface IInputSystem
+    {
+        double FetchDoubleValue(string message);
+        string FetchStringValue(string message);
+        int FetchIntValue(string message);
+    }
+}
